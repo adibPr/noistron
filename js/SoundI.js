ff1913cc87700c731617e939b0bd66d07a5099e6
Adib Pratama <pratama.adib@gmail.com>
2017-07-20 16:24:39 +0700

    first working version, play stop button, slider and event sync

---
+++ js/SoundI.js
@@ -0,0 +1,19 @@
+var Sound = Class.create({
+  initialize: function (sound_path) {
+    this.sound = new Audio (sound_path);
+    this.sound.loop = true;
+    this.sound.volume = 0.5;
+  },
+  play: function() {
+    this.sound.play ();
+  },
+  set_volume: function (vol) {
+    this.sound.volume = vol;
+  },
+  get_volume: function () {
+    return this.sound.volume;
+  },
+  stop: function () {
+    this.sound.pause ();
+  }
+});
--- js/renderer.js
+++ js/renderer.js
@@ -1,3 +1,37 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+function prepare_sound (sound_dom) {
+  sound_name = sound_dom.attributes['data-id'].value;
+  // create object of sound
+  var this_sound = new Sound ('../sound/' + sound_name + '.mp3');
+
+  // add event
+  sound_dom.querySelector ('.play').addEventListener ('click', ()=> this_sound.play ());
+  sound_dom.querySelector ('.stop').addEventListener ('click', ()=> this_sound.stop ());
+
+  // create slider
+  var slider = sound_dom.querySelector ('.slider');
+  noUiSlider.create (slider, {
+    start : this_sound.get_volume () * 100,
+    range : {
+      'min' : 0,
+      'max' : 100
+    },
+    connect: true
+  });
+
+
+  // set event
+  slider.noUiSlider.on ('change', function (values, handle) {
+    this_sound.set_volume (values / 100);
+  });
+
+}
+
+var sound_div = document.querySelectorAll ('.sound');
+for (i=0; i<sound_div.length; i++) {
+  var sound_dom = sound_div[i];
+  prepare_sound (sound_dom);
+}
